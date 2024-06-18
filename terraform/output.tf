f13d8556e99294b8fb70c0303a1b3fbaa1687d1e
mhsy <hasyim11c@gmail.com>
2024-06-18 22:35:13 +0700

    [ansible; terraform] refactor: change output terra and delete some var ansi

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,10 +2,6 @@ output "ec2_monitoring_ip" {
   value = module.ec2.ec2_monitoring_server_ip
 }
 
-output "ec2_web_server_ip" {
-  value = module.ec2.ec2_web_server_ip
-}
-
 output "alb_dns" {
   value = module.alb.load_balancer_dns_name
 }
